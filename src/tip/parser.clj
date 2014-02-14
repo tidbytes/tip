c0b516560e056740b3e0b97cdc9d677434dd72ec
emiln <emil@nauerby.com>
2014-02-14 23:42:55 +0100

    Clean up `tip.parser`
    This edition uses the prettier docstring available for `def` as of Clojure 1.3, and also removes the rather pointless definition of `ebnf`. If it becomes needed, it can easily be reintroduced.

--- src/tip/parser.clj
+++ src/tip/parser.clj
@@ -2,15 +2,9 @@
   (:require [instaparse.core :refer []]))
 
 (def
-  ^{:private true}
-  ebnf
-  (slurp "src/tip/tip.ebnf"))
-
-(def
-  ^{:doc
-  	"The main function of `tip.parser`. This is the actual parser, and it is
-  	 called with a string containing a TIP source code program.
-
-  	 Example: (parser \"inc(n) { return n+1; }\""}
   parser
-  (instaparse.core/parser ebnf))
+  "The main function of `tip.parser`. This is the actual parser, and it is
+   called with a string containing a TIP source code program.
+
+   Example: (parser \"inc(n) { return n+1; }\""
+  (instaparse.core/parser (slurp "src/tip/tip.ebnf")))
